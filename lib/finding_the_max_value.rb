d225e66151effdbbbc90de55a46919cc67260d5f
Kevin Neyer <kevinneyer@gmail.com>
2020-04-07 23:10:59 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -4,7 +4,7 @@ def find_max_value(array)
   max = -1
   while counter < array.length do
     if max < array[counter]
-      max = counter
+      max = array[counter]
     end
     counter += 1 
   end
